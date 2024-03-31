7fe4bd0e07dc2435e427bc9263aaa85d1086043b
Shreyas <shreyastungar762@gmail.com>
2024-04-01 00:40:42 +0530

    Page 4, 5 and footer structure compleated

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function loadingAnimation(){
         opacity:0
     });
 
-    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero3 span ,#hero4 h1",{
         y:150,
         stagger:0.2,
     });
